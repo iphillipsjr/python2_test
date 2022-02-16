dfb77aa650017429bfdacc499ae253988af7a9dd
iphillipsjr <iphillipsjr@yahoo.com>
2022-02-15 20:41:50 -0500

    Additional java file chapter 1

---
+++ java_test/HelloWorld10.java
@@ -0,0 +1,13 @@
+import javax.swing.plaf.synth.SynthStyle;
+
+/**
+ * HelloWorld10
+ */
+public class HelloWorld10 {
+public static void main(String[] args) {
+    for (int index = 0; index < 10; index++) {
+        System.out.println("Hello, World");
+    }
+}
+    
+}
---
+++ java_test/UseThree.java
@@ -0,0 +1,13 @@
+/**
+ * UseThree
+ */
+public class UseThree {
+public static void main(String[] args) {
+    System.out.print(args[2]);
+    System.out.print(", ");
+    System.out.print(args[1]);
+    System.out.print(", and ");
+    System.out.println(args[0]);
+}
+    
+}
